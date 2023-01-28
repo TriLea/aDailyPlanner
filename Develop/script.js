c369d0aee3b8221d306ae42a48c2bfa3bc01a334
trista_lea <tristan_lea@outlook.com>
2023-01-27 23:20:04 -0800

    fixed it, put script at bottom, got date logged, ready to flesh out logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,11 @@
 // moment js was not working so I used dayjs instead
-const dayjs = require('dayjs');
-var now = dayjs;;
-now.format(dddd,h,m);
+var now = dayjs();
+now.format("dddd,h,m");
 
-console.lo(now.toString());
+console.log(now);
 
 var text = $('.textarea');
 var saveBtn = $('.saveBtn');
-var hour = moment().hour();
+var hour = dayjs().hour();
 
 // saveBtn.on('click', function() {
