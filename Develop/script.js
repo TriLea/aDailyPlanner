67d15cfdede4339e1be13507982320145078a255
trista_lea <tristan_lea@outlook.com>
2023-02-05 22:57:40 -0800

    just added some comments and skeleton code for what i would like to do

--- Develop/script.js
+++ Develop/script.js
@@ -8,4 +8,31 @@ var text = $('.textarea');
 var saveBtn = $('.saveBtn');
 var hour = dayjs().hour();
 
-// saveBtn.on('click', function() {
+//need dom hooks
+
+function save()
+{
+    //save button onclick event
+    //how to do in jquery syntax?
+}
+
+function saveToLocalStorage()
+{
+    //take from textarea?
+}
+
+function resetTextArea()
+{
+    //resets text area on page load
+}
+
+function displayColors()
+{
+    //updates color after each event, //and or change in hour?
+    //clear button
+    for (let i = 0; i < 9; i++) 
+    {
+        //should be for each row instead
+    }
+}
+
